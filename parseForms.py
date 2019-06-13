deea94391181b261046fe98b723e50f68abdbb73
zyc <billyzyc65535@gmail.com>
2019-06-13 20:09:05 +0800

    link field is an array that might be empty

--- parseForms.py
+++ parseForms.py
@@ -2,8 +2,8 @@ from shortener import shorten
 
 def parseBiggerlabCourseFeedback(blob):
 
-    def deDownload(link):
-        return link[:-9] if link.endswith("&download") else link
+    def deDownload(linkArr):
+        return "" if len(linkArr) == 0 else (linkArr[0][:-9] if linkArr[0].endswith("&download") else linkArr[0])
     # 有些link最后会带一个"&download" 这会导致它默认下载而不是预览，所以我们扔掉它。
 
     info = {}
@@ -20,7 +20,7 @@ def parseBiggerlabCourseFeedback(blob):
     info["courseEndDate"] = blob["field_12"] # 课程结束日期
     info["courseContent"] = blob["field_13"] # 已上课程内容
     info["courseCapturelink"] = blob["field_32"] # 课程视频链接
-    info["projectScreenshot"] = shorten(deDownload(blob["field_18"][0])) # 项目作品截图
+    info["projectScreenshot"] = shorten(deDownload(blob["field_18"])) # 项目作品截图
 
     info["rateAttendance"] = blob["field_14"] # 学生课堂参与度
     info["rateUnderstanding"] = blob["field_15"] # 知识点理解情况
@@ -39,20 +39,20 @@ def parseBiggerlabCourseFeedback(blob):
     info["projectDescription"] = blob["field_42"] # 学生项目描述/所学内容
     info["teacherNotes"] = blob["field_46"] # 导师对该学生的评语
 
-    info["awardEvidence"] = shorten(deDownload(blob["field_40"][0])) # 竞赛获奖证明截图
+    info["awardEvidence"] = shorten(deDownload(blob["field_40"])) # 竞赛获奖证明截图
     info["competitionName"] = blob["field_34"] # 比赛名称
     info["prizeReceived"] = blob["field_35"] # 获得的奖项
 
-    info["projectPublishEvidence"] = shorten(deDownload(blob["field_47"][0])) # 项目发布证明截图
-    info["copyrightEvidence"] = shorten(deDownload(blob["field_48"][0])) # 著作权证明截图
+    info["projectPublishEvidence"] = shorten(deDownload(blob["field_47"])) # 项目发布证明截图
+    info["copyrightEvidence"] = shorten(deDownload(blob["field_48"])) # 著作权证明截图
     info["projectLink"] = blob["field_43"] # 项目链接
-    info["sourceCode"] = shorten(deDownload(blob["field_36"][0])) # 源代码文件
+    info["sourceCode"] = shorten(deDownload(blob["field_36"])) # 源代码文件
 
-    info["admissionEvidence"] = shorten(deDownload(blob["field_49"][0])) # 名校录取证明截图
+    info["admissionEvidence"] = shorten(deDownload(blob["field_49"])) # 名校录取证明截图
     info["schoolName"] = blob["field_44"] # 学校名称
     info["profession"] = blob["field_45"] # 专业名称
 
-    info["awardOthers"] = shorten(deDownload(blob["field_50"][0])) # 其他荣誉证明截图
+    info["awardOthers"] = shorten(deDownload(blob["field_50"])) # 其他荣誉证明截图
     return info
 
 translation = {
