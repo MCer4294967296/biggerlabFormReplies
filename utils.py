099d7f1c6fa9491e8559ad1c246333cdd081c31f
zyc <billyzyc65535@gmail.com>
2019-06-21 13:37:54 +0800

    put stuff into utils file

---
+++ utils.py
@@ -0,0 +1,54 @@
+def multiThreadMap(job, collection, threadCount = os.cpu_count()):
+    tCount = 1
+    id = 0
+    while tCount < threadCount:
+        pid = os.fork()
+        tCount += 1
+        if pid == 0:
+            id = tCount - 1
+            break
+    start = int(len(collection) * id / threadCount)
+    end = int(len(collection) * (id + 1) / threadCount)
+    for i in range(start, end):
+        job(collection[i])
+    if id != 0:
+        sys.exit(0)
+
+def getIDList(docs, count=10, idStart=None, idEnd=None, key="_id"):
+    '''returns a list of documents with the provided ID range and count,
+    as well as the previous and next possible ID that's outside of the returned
+    range. If there are no more, None is returned. '''
+
+    if idStart or idEnd:
+        try:
+            idEnd = int(idEnd) if idEnd else None
+            idStart = int(idStart) if idStart else None
+        except:
+            return []
+
+    chosen = []
+    if idStart and idEnd:
+        chosen = list(docs.where("this['{key}'] >= {idStart} && this['{key}'] <= {idEnd}").format(idStart=idStart, idEnd=idEnd, key=key).sort(key, pymongo.DESCENDING))
+        prevID = idStart - 1
+        nextID = idEnd + 1
+    elif idStart:
+        match = list(docs.where("this['{key}'] >= {idStart}".format(idStart=idStart, key=key)).sort(key, pymongo.DESCENDING))
+        chosen = match[-10:]
+        prevID = idStart - 1
+        nextID = chosen[0][key] + 1 if len(match) > 10 else None
+    elif idEnd:
+        match = list(docs.where("this['{key}'] <= {idEnd}".format(idEnd=idEnd, key=key)).sort(key, pymongo.DESCENDING))
+        chosen = match[:10]
+        prevID = chosen[-1][key] - 1 if len(match) > 10 else None
+        nextID = idEnd + 1
+    else:
+        match = list(docs.sort(key, pymongo.DESCENDING))
+        chosen = match[:10]
+        prevID = chosen[-1][key] - 1 if len(match) > 10 else None
+        nextID = None
+
+    return chosen, prevID, nextID
+
+def handlerSIGINT(signal, frame):
+    itchat.logout()
+    sys.exit(0)
--- wechat-auto.py
+++ wechat-auto.py
@@ -3,20 +3,18 @@ from flask import Flask, abort, request, render_template, jsonify, Response
 from flask_cors import CORS
 import pymongo, itchat
 import parseForms, templates, formNameTranslation, metaInitializer
+from utils import *
 
 
 app = Flask(__name__)
 CORS(app)
-
-def handlerSIGINT(signal, frame):
-    itchat.logout()
-    sys.exit(0)
 
 signal.signal(signal.SIGINT, handlerSIGINT)
 
 
 @app.route("/jinshujuIN", methods=["POST"])
 def jinshujuIN():
+    # 群组test只有一个群的时候，搜索出错。
     if not request.is_json:
         return "400 BAD REQUEST: Data is not a json, rejecting.", 400
 
@@ -25,11 +23,6 @@ def jinshujuIN():
     form = formNameTranslation.translation[jsonObj["form"]] # get the unique form name
     id = int(jsonObj["entry"]["serial_number"])
 
-    # bot = Bot(console_qr=True, cache_path=True)
-    # 群组不活跃的情况下，群组搜索出错。
-    # 群组test只有一个群的时候，搜索出错。
-    # 群组搜索下如何命名微信群
-    # target_group = bot.groups(update=True, contact_only=False).search('test')[0]
 
     parser = parseForms.translation[form] # from the unique form name, we get the parser.
     info = parser(jsonObj["entry"]) # parse the information out,
@@ -56,25 +49,6 @@ def jinshujuIN():
     #itchat.send_msg(msg=message, toUserName="filehelper")
     #itchat.send_msg(msg=message, toUserName=itchat.search_friends(name="Rock大石头")[0]["UserName"])
 
-    """
-    groupName = 'BP-' + beforeSend['field_2'] +'-'
-    group = itchat.search_chatrooms(name=groupName)
-    if group:
-        itchat.send_msg(msg = afterSend,toUserName=getroom_message(groupName))
-    else:
-        itchat.send('Could not find the group', toUserName='filehelper')
-        itchat.send_msg(msg = afterSend, toUserName='filehelper')
-    #print (data + ' sendCompleted')
-    #避免微信发消息过于频繁，延时
-    time.sleep(0.5)
-
-    #1小时后itchat刷新，维持在线状态
-
-    #afterSend未定义，if判断未执行状态下，会报错建议写入if条件下
-    #target_group.send(afterSend)
-    #bot.join()
-    """
-
 
 @app.route("/sendToWechat", methods=["POST"])
 def sendToWechat():
@@ -100,17 +74,21 @@ def sendToWechat():
         else:
             userNameList.append(itchat.search_chatrooms(name=target)[0]["UserName"])
 
-    col = db[form] # access the database
-    info = col.find({"_id": id})[0] # retrieve the information
-
-    message = jsonObj["message"]
+    mCol = db["meta" + form]
+    try:
+        message = jsonObj["message"]
+    except:
+        message = mInfo["message"]
+        if message == "":
+            message = genMessage(form, id)
+            mCol.update_one({"jsjid": id}, {"$set": {"message": message}})
+            
     for userName in userNameList:
         itchat.send(msg=message, toUserName=userName)
     # target = "filehelper" # placeholder, should be from info
 
     # itchat.send(msg=message, toUserName=target) # send the message
-    meta = db["meta" + form]
-    meta.update_one({'jsjid': id}, {'$set': {'sentToWechat': True}}) # update the database
+    mCol.update_one({'jsjid': id}, {'$set': {'sentToWechat': True}}) # update the database
 
     return "200 OK", 200
 
@@ -125,30 +103,10 @@ def getPage(form=""):
             idStart = int(idStart) if idStart else None
         except:
             return "400 BAD REQUEST: id is not valid.", 400
-
     col = db[form]
     docs = col.find()
-    chosen = []
-    if idStart and idEnd:
-        chosen = list(docs.where("this['_id'] >= {idStart} && this['_id'] <= {idEnd}").format(idStart=idStart, idEnd=idEnd).sort("_id", pymongo.DESCENDING))
-        prevID = idStart - 1
-        nextID = idEnd + 1
-    elif idStart:
-        match = list(docs.where("this['_id'] >= {idStart}".format(idStart=idStart)).sort("_id", pymongo.DESCENDING))
-        chosen = match[-10:]
-        prevID = idStart - 1
-        nextID = chosen[0]["_id"] + 1 if len(match) > 10 else None
-    elif idEnd:
-        match = list(docs.where("this['_id'] <= {idEnd}".format(idEnd=idEnd)).sort("_id", pymongo.DESCENDING))
-        chosen = match[:10]
-        prevID = chosen[-1]["_id"] - 1 if len(match) > 10 else None
-        nextID = idEnd + 1
-    else:
-        match = list(docs.sort("_id", pymongo.DESCENDING))
-        chosen = match[:10]
-        prevID = chosen[-1]["_id"] - 1 if len(match) > 10 else None
-        nextID = None
-
+    chosen, prevID, nextID = getIDList(docs, idStart=idStart, idEnd=idEnd)
+    
     meta = db["meta" + form]
     leftList = []
     for i in range(len(chosen)):
@@ -249,27 +207,11 @@ def lc():
     multiThreadMap(func, dirContent)
 
 
-def multiThreadMap(job, collection, threadCount = os.cpu_count()):
-    tCount = 1
-    id = 0
-    while tCount < threadCount:
-        pid = os.fork()
-        tCount += 1
-        if pid == 0:
-            id = tCount - 1
-            break
-    start = int(len(collection) * id / totalThreads)
-    end = int(len(collection) * (id + 1) / totalThreads)
-    for i in range(start, end):
-        job(collection[i])
-    if id != 0:
-        sys.exit(0)
-
 def ec():
-    for f in os.listdir("static/wechatStuff"):
-        if f != "QR.png":
-            os.remove("static/wechatStuff/{}".format(f))
-    os.remove("QR.png")
+    try:
+        os.remove("QR.png")
+    except FileNotFoundError:
+        pass
 
 
 @app.route("/login", methods=["GET"])
@@ -302,6 +244,7 @@ if __name__ == '__main__':
             sleep(rand.random() + 1)
         lastSentMsgTimestamp = time.time()
         self.send(msg, toUserName, mediaId)
+
     itchat.send = send
     print("---Server has started---") # ¯¯¯¯\_(ツ)_/¯¯¯¯
     app.run(host='0.0.0.0', port=5050, debug=True, use_reloader=False)
