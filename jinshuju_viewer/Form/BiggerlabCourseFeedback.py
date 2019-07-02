879f1745afd2fc1ae2ecb2ca015f016598be622d
zyc <billyzyc65535@gmail.com>
2019-07-03 00:58:46 +0800

    Add '/getDocs' route Used to getting documents given criteria.
    Documentation improvement.

--- jinshuju_viewer/Form/BiggerlabCourseFeedback.py
+++ jinshuju_viewer/Form/BiggerlabCourseFeedback.py
@@ -1,4 +1,4 @@
-import functools, json, requests
+import datetime, functools, json, requests
 from flask import (Blueprint, flash, g, jsonify, make_response, redirect,
                    render_template, request, session, url_for)
 import pymongo
@@ -17,9 +17,12 @@ class BiggerlabCourseFeedback(form.ToWechatForm):
     @staticmethod
     @bp.route("/", methods=["GET"])
     def getPage():
-        bots = utils.getActiveBots(main.app.config["WECHATBOTSERVER"])
+        '''The renderer of the viewer page. 
+        :returns: the rendered page or 400.
+        '''
+        bots = utils.getActiveBots(main.app.config["WECHATBOTSERVER"]) # get the alive bots
         for bot in bots:
-            bot["HeadSource"] = "{}static/{}.png".format(main.app.config["WECHATBOTSERVER"], bot["NickName"])
+            bot["HeadSource"] = "{}static/{}.png".format(main.app.config["WECHATBOTSERVER"], bot["NickName"]) # get the bots a head image.
         
         idStart = request.args.get("idStart")
         idEnd = request.args.get("idEnd")
@@ -29,27 +32,34 @@ class BiggerlabCourseFeedback(form.ToWechatForm):
                 idStart = int(idStart) if idStart else None
             except:
                 return make_response(("id is not valid.", 400))
+        # parse out the id range limiters.
 
-        docs = BiggerlabCourseFeedback.col.find()
-        chosen, prevID, nextID = utils.getIDList(docs, idStart=idStart, idEnd=idEnd)
+        docs = BiggerlabCourseFeedback.col.find() # query the database,
+        chosen, prevID, nextID = utils.getIDList(docs, idStart=idStart, idEnd=idEnd) # and choose documents as wanted.
 
-        leftList = []
-        for i in range(len(chosen)):
+        leftList = [] # initialize the message list on the left.
+        for i in range(len(chosen)): # for each chosen document,
             doc = chosen[i]
             id = doc["_id"]
             mInfo = BiggerlabCourseFeedback.mCol.find({"jsjid": id})[0]
             item = {"id": id, "studentName": doc["studentName"] + (" (已发送)" if mInfo["sentToWechat"] else "")}
+            # construct the individual item.
             leftList.append(item)
 
         prevLink = "{base_url}?idEnd={prevID}".format(base_url=request.base_url, prevID=prevID) if prevID is not None else None
         nextLink = "{base_url}?idStart={nextID}".format(base_url=request.base_url, nextID=nextID) if nextID is not None else None
-
+        # calculate the previous and next link's id range limiter.
+        g.WECHATBOTSERVER = main.app.config["WECHATBOTSERVER"]
         return render_template("Form/BiggerlabCourseFeedback.html", prevLink=prevLink, nextLink=nextLink, leftList=leftList, bots=bots, WECHATBOTSERVER=main.app.config["WECHATBOTSERVER"])
+        #return render_template("wechatted.html", prevLink=prevLink, nextLink=nextLink, leftList=leftList, bots=bots, WECHATBOTSERVER=main.app.config["WECHATBOTSERVER"])
 
 
     @staticmethod
     @bp.route("/jinshujuIN", methods=["POST"])
     def jinshujuIN():
+        '''the jinshuju data handler.
+        :returns: A helpful message and a status code.
+        '''
         if not request.is_json:
             logging.warning("/jinshujuIN received non-json data.")
             return make_response(("Data is not a json, rejecting.", 400))
@@ -90,12 +100,74 @@ class BiggerlabCourseFeedback(form.ToWechatForm):
         if message == "":
             message = BiggerlabCourseFeedback.genMessage(id)
             BiggerlabCourseFeedback.mCol.update_one({"jsjid": id}, {"$set": {"message": message}})
+        BiggerlabCourseFeedback.mCol.update_one({"jsjid": id}, {"$set": {"viewed": True}})
         return jsonify({"id": id,
                         "message": message,
                         "studentName": info["studentName"],
                         "teacherName": info["teacherName"],
                         "reasonFilling": info["reasonFilling"],
                         "messageEdited": mInfo["edited"]})
+
+
+    @staticmethod
+    @bp.route("/getDocs", methods=["POST"])
+    def getDocs():
+        if request.is_json:
+            data = request.json
+        else:
+            data = request.args
+        
+        idStart = data.get("idStart", None)
+        idEnd = data.get("idEnd", None)
+        timeFilledStart = data.get("timeFilledStart", None)
+        timeFilledEnd = data.get("timeFilledEnd", None)
+        reasonFillingList = data.get("reasonFillingList", None)
+        count = int(data.get("count", 100))
+        offset = int(data.get("offset", 0))
+
+        queryString = "true"
+        if idStart:
+            queryString += "&& this['_id'] >= {idStart}".format(idStart=idStart)
+            # docs = docs.where("this['_id'] >= {idStart}".format(idStart=idStart))
+        if idEnd:
+            queryString += "&& this['_id'] <= {idEnd}".format(idEnd=idEnd)
+            # docs = docs.where("this['_id'] <= {idEnd}".format(idEnd=idEnd))
+        docs = BiggerlabCourseFeedback.col.find().where(queryString)
+        chosen = []
+        while True:
+            try:
+                chosen.append(docs.next())
+            except StopIteration:
+                break
+        mQueryString = "true"
+        if timeFilledStart:
+            mQueryString += "&& this['timeFilled'] >= {timeFilledStart}".format(timeFilledStart=timeFilledStart)
+            # docs = docs.where("this['timeFilled'] >= {timeFilledStart}".format(timeFilledStart=timeFilledStart))
+        if timeFilledEnd:
+            mQueryString += "&& this['timeFilled'] <= {timeFilledEnd}".format(timeFilledEnd=timeFilledEnd)
+            # docs = docs.where("this['timeFilled'] <= {timeFilledEnd}".format(timeFilledEnd=timeFilledEnd))
+        print(mQueryString)
+        mDocs = BiggerlabCourseFeedback.mCol.find().where(mQueryString)
+        mChosen = []
+        while True:
+            try:
+                mChosen.append(mDocs.next())
+            except StopIteration:
+                break
+        #chosen = list(docs.where(queryString))
+        
+        if reasonFillingList:
+            for doc in list(chosen):
+                if doc["reasonFilling"] not in reasonFillingList:
+                    chosen.remove(doc)
+        
+        mChosenID = [mDoc["jsjid"] for mDoc in mChosen]
+        for doc in list(chosen):
+            if doc["_id"] not in mChosenID:
+                chosen.remove(doc)
+
+        chosen = chosen[offset:count+offset]
+        return jsonify(chosen)
 
 
     @staticmethod
@@ -167,32 +239,16 @@ class BiggerlabCourseFeedback(form.ToWechatForm):
         mInfo["sentToWechat"] = False
         mInfo["message"] = ""
         mInfo["edited"] = False
+        mInfo["timeFilled"] = datetime.datetime.now()
 
         return mInfo
 
-    
-    messageTemplates = {
-        "试听课反馈，由课程顾问复述给家长" :
-"""{studentName}家长您好，导师{teacherName}对您的孩子于{courseStartDate}试听的{courseName}作出了反馈。
-课堂参与度：{rateAttendance}
-知识点理解情况：{rateUnderstanding}
-课堂任务完成情况：{rateAssignmentCompletion}
-综合：{rateGeneral}
 
-这是您的孩子所学的课程内容：{courseContent}
-您可以点击{projectScreenshot}来查看您孩子的作品。
-这是导师对您的孩子作出的评价：{teacherComment}""",
-        "月度课程反馈，给家长" : 
-"""{studentName}家长您好，导师{teacherName}对您的孩子于{courseStartDate}开始的{courseName}作出了月度课程反馈。
-课堂参与度：{rateAttendance}
-知识点理解情况：{rateUnderstanding}
-课堂任务完成情况：{rateAssignmentCompletion}
-综合：{rateGeneral}
-
-您可以点击{projectScreenshot}来查看您的孩子的项目进展。
-这是您的孩子最近正在学的课程内容：{courseContent}
-这是导师对您的孩子作出的评价：{teacherComment}""",
-        "阶段性+续费课程反馈，给家长" :
+    @staticmethod
+    def messageTemplatesTmp(**info):
+        reasonFilling = info["reasonFilling"]
+        if reasonFilling == "阶段性+续费课程反馈，给家长":
+            template = \
 """{studentName}家长您好，导师{teacherName}对您的孩子于{courseStartDate}开始的{courseName}作出了反馈。
 课堂参与度：{rateAttendance}
 知识点理解情况：{rateUnderstanding}
@@ -202,18 +258,64 @@ class BiggerlabCourseFeedback(form.ToWechatForm):
 您可以点击{projectScreenshot}来查看您的孩子的项目进展。
 这是您的孩子最近正在学的课程内容：{courseContent}
 这是导师对您的孩子作出的评价：{teacherComment}"""
-    }
+        elif reasonFilling == "月度课程反馈，给家长":
+            template = \
+"""{studentName}家长您好，导师{teacherName}对您的孩子于{courseStartDate}开始的{courseName}作出了月度课程反馈。
+课堂参与度：{rateAttendance}
+知识点理解情况：{rateUnderstanding}
+课堂任务完成情况：{rateAssignmentCompletion}
+综合：{rateGeneral}
+
+您可以点击{projectScreenshot}来查看您的孩子的项目进展。
+这是您的孩子最近正在学的课程内容：{courseContent}
+这是导师对您的孩子作出的评价：{teacherComment}"""
+        elif reasonFilling == "试听课反馈，由课程顾问复述给家长":
+            template = \
+"""{studentName}家长您好，导师{teacherName}对您的孩子于{courseStartDate}试听的{courseName}作出了反馈。
+课堂参与度：{rateAttendance}
+知识点理解情况：{rateUnderstanding}
+课堂任务完成情况：{rateAssignmentCompletion}
+综合：{rateGeneral}
+
+这是您的孩子所学的课程内容：{courseContent}
+您可以点击{projectScreenshot}来查看您孩子的作品。
+这是导师对您的孩子作出的评价：{teacherComment}"""
+        elif reasonFilling == "学生获奖/录取/荣誉反馈":
+            awardType = info["awardType"]
+            if awardType == "竞赛获奖":
+                template = \
+"""{studentName}家长您好，您的孩子在{competitionName}中获得了{prizeReceived}的成绩！这是获奖证明{awardEvidence}
+这是您孩子所做的项目：{projectDescription}
+这是{teacherName}导师对您孩子的评语：{teacherNotes}"""
+            elif awardType == "项目发布":
+                template = \
+"""{studentName}家长您好，您的孩子发布了自己的项目{projectLink}！这是项目发布证明{projectPublishEvidence}
+这是您孩子所做的项目：{projectDescription}
+这是{teacherName}导师对您孩子的评语：{teacherNotes}"""
+            elif awardType == "名校录取":
+                template = \
+"""{studentName}家长您好，您的孩子被{schoolName}的{profession}录取了！这是录取通知书{admissionEvidence}
+这是{teacherName}导师对您孩子的评语：{teacherNotes}"""
+            elif awardType == "著作权":
+                template = \
+"""{studentName}家长您好，您的孩子申请著作权成功了！这是登记证书{copyrightEvidence}
+这是您孩子项目的截图：{projectScreenshot}
+这是{teacherName}导师对您孩子的评语：{teacherNotes}"""
+            else:
+                return ""
+        else:
+            return ""
+
+        return template.format(**info)
+
 
 
     @staticmethod
     def genMessage(id):
-        
         info = BiggerlabCourseFeedback.col.find({"_id": id})[0]
 
-        if info["reasonFilling"] not in BiggerlabCourseFeedback.messageTemplates.keys():
-            return "" # it was "other"
-
-        message = BiggerlabCourseFeedback.messageTemplates[info["reasonFilling"]].format(**info)
+        #message = BiggerlabCourseFeedback.messageTemplates[info["reasonFilling"]].format(**info)
+        message = BiggerlabCourseFeedback.messageTemplatesTmp(**info)
         return message
 
 
@@ -226,7 +328,7 @@ class BiggerlabCourseFeedback(form.ToWechatForm):
 
         bots = utils.getActiveBots(main.app.config["WECHATBOTSERVER"])
         if not bots:
-            return make_response(("There are no bots alive.", 400))
+            return make_response(("There are no bots alive.", 403))
         
         try:
             targetList = request.json["targetList"]
